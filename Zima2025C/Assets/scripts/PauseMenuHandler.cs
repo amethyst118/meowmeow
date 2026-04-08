519537acd8701132ffa6d08c0673deea962266e7
wiwiwi <waaaaaah>
2026-04-08 12:31:12 +0200

    Slider
    slider

--- Zima2025C/Assets/scripts/PauseMenuHandler.cs
+++ Zima2025C/Assets/scripts/PauseMenuHandler.cs
@@ -1,9 +1,13 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenuHandler : MonoBehaviour
 {
     private GameStateManager myGameStateManagerRef;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public Slider MySlider;
+    public TMP_Text MyText;
     void Start()
     {
         var goGameManagerRef = GameObject.FindGameObjectWithTag("GameController");
@@ -17,5 +21,19 @@ public class PauseMenuHandler : MonoBehaviour
     public void OnExitButton_Clicked()
     {
         Application.Quit();
+
     }
+
+    public void OnValue_Changed()
+    {
+        MyText.text = MySlider.value.ToString("F2");
+
+    }
+
+    public void OnBalueWithParameter_Changed(float value)
+    {
+        MyText.text = value.ToString("F2");
+    }
+
+
 }
